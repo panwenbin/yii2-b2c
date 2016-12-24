00a74cdd4d1265b6a3b2715b622dffc1bb8fabde
Pan Wenbin <panwenbin@gmail.com>
2016-12-24 18:56:48 +0800

    调整用户列表显示字段

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -26,12 +26,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
             'id',
             'username',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
-            // 'email:email',
-            // 'status',
-            // 'created_at',
+//            'auth_key',
+//            'password_hash',
+//            'password_reset_token',
+             'email:email',
+             'status',
+             'created_at:datetime',
             // 'updated_at',
 
             ['class' => 'yii\grid\ActionColumn'],
